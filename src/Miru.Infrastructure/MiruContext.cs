5062d09ceaa32eab540aec362ca2a3ebaaf83ffd
Phantom-Whisper <gabrielcoudel35@gmail.com>
2025-12-22 23:10:39 +0100

    :construction: base of DbContext

---
+++ src/Miru.Infrastructure/MiruContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Miru.Domain;
+
+namespace Miru.Infrastructure
+{
+    public class MiruContext : DbContext
+    {
+        public MiruContext(DbContextOptions<MiruContext> options)
+            : base(options)
+        { }
+
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+        public DbSet<UserMedia> UserMedias => Set<UserMedia>();
+        public DbSet<SeasonEntity> Seasons => Set<SeasonEntity>();
+        public DbSet<EpisodeEntity> Episodes => Set<EpisodeEntity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<MediaEntity>()
+                .HasDiscriminator<string>("MediaType")
+                .HasValue<MovieEntity>("Movie")
+                .HasValue<SerieEntity>("Series");
+
+            modelBuilder.Entity<UserMedia>()
+                .HasKey(um => new { um.UserId, um.MediaId });
+        }
+    }
+}
