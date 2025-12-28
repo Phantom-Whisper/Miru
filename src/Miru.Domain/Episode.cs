4d62e498f9c3473a405d3c66c5684d7fb7343e0b
Gabriel <157543647+Phantom-Whisper@users.noreply.github.com>
2025-12-28 03:29:02 +0100

    Feature/ef entities (#5)
    * :construction: beginning of Entities
    
    * :card_file_box: corrections of types
    
    * :construction: base of entities done
    
    * :construction: base of DbContext
    
    * :recycle: for better entities
    
    * :technologist: add assembly file
    
    * :white_check_mark: entities XUnit tests

---
+++ src/Miru.Domain/Episode.cs
@@ -0,0 +1,54 @@
+﻿namespace Miru.Domain
+{
+    public class Episode
+    {
+        /// <summary>
+        /// Episode number within the season.
+        /// </summary>
+        public int EpisodeNumber { get; private set; }
+
+        /// <summary>
+        /// Episode title.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Dureation of the episode in minutes.
+        /// </summary>
+        public TimeSpan Duration { get; private set; }
+
+        /// <summary>
+        /// Identifier of the season this episode belongs to.
+        /// </summary>
+        public Guid SeasonId { get; private set; }
+
+        /// <summary>
+        /// Default constructor for ORM.
+        /// </summary>
+        protected Episode() { }
+
+        /// <summary>
+        /// Constructor creating a new Episode.
+        /// </summary>
+        /// <param name="episodeNumber">Episode number.</param>
+        /// <param name="title">Title of the episode.</param>
+        /// <param name="duration">Duration of the episode.</param>
+        /// <param name="seasonId">Season identifier.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if the episode number is lower than zero.</exception>
+        /// <exception cref="ArgumentException">Thrown if title is null or empty, or if duration is less than zero.</exception>
+        /// <exception cref="ArgumentException">Thrown if duration is less than or equal to zero.</exception>
+        public Episode(int episodeNumber, string title, TimeSpan duration, Guid seasonId)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(episodeNumber);
+            if (string.IsNullOrWhiteSpace(title)) 
+                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+            if (duration <= TimeSpan.Zero) 
+                throw new ArgumentException("Duration must be positive.", nameof(duration));
+
+            EpisodeNumber = episodeNumber;
+            Title = title;
+            Duration = duration;
+            SeasonId = seasonId;
+        }
+    }
+}
