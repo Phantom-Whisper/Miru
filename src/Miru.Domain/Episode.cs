eb4d9a5a2e98b147a8e22bff6047c4afe2538469
Phantom-Whisper <gabrielcoudel35@gmail.com>
2026-01-16 17:46:01 +0100

    :ambulance: Simplify domain entities

--- src/Miru.Domain/Episode.cs
+++ src/Miru.Domain/Episode.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Episode title.
         /// </summary>
-        public string Title { get; private set; }
+        public string Title { get; private set; } = null!;
 
         /// <summary>
         /// Dureation of the episode in minutes.
@@ -26,29 +26,5 @@
         /// Default constructor for ORM.
         /// </summary>
         protected Episode() { }
-
-        /// <summary>
-        /// Constructor creating a new Episode.
-        /// </summary>
-        /// <param name="episodeNumber">Episode number.</param>
-        /// <param name="title">Title of the episode.</param>
-        /// <param name="duration">Duration of the episode.</param>
-        /// <param name="seasonId">Season identifier.</param>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown if the episode number is lower than zero.</exception>
-        /// <exception cref="ArgumentException">Thrown if title is null or empty, or if duration is less than zero.</exception>
-        /// <exception cref="ArgumentException">Thrown if duration is less than or equal to zero.</exception>
-        public Episode(int episodeNumber, string title, TimeSpan duration, Guid seasonId)
-        {
-            ArgumentOutOfRangeException.ThrowIfNegative(episodeNumber);
-            if (string.IsNullOrWhiteSpace(title)) 
-                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
-            if (duration <= TimeSpan.Zero) 
-                throw new ArgumentException("Duration must be positive.", nameof(duration));
-
-            EpisodeNumber = episodeNumber;
-            Title = title;
-            Duration = duration;
-            SeasonId = seasonId;
-        }
     }
 }
