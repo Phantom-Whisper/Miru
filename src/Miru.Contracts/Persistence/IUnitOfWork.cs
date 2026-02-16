b3b170044989ca9a12bd7ad2cb755a2a18ecaa4f
Phantom-Whisper <gabrielcoudel35@gmail.com>
2026-02-16 03:06:42 +0100

    :construction: added uow interface & implementation

---
+++ src/Miru.Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using Miru.Contracts.Repositories;
+
+namespace Miru.Contracts.Persistence;
+
+public interface IUnitOfWork : IDisposable
+{
+    IMovieRepository Movies { get; }
+    ISerieRepository Series { get; }
+    ISeasonRepository Seasons { get; }
+    IEpisodeRepository Episodes { get; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task RejectChangesAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+}
