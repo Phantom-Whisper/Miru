b7088b176791465c5bd86a4ce09a6b429a5517c4
Phantom-Whisper <gabrielcoudel35@gmail.com>
2026-05-11 00:52:08 +0200

    feat: UI framework

---
+++ src/Miru.Web/src/components/Hero/Hero.tsx
@@ -0,0 +1,18 @@
+
+function Hero() {
+    return (
+        <section>
+            <div className="mb-8">
+                <h2 className="text-4xl font-bold mb-2">
+                    Hello Alex 👋
+                </h2>
+
+                <p className="text-zinc-400 text-lg">
+                    Pick up where you left off
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
